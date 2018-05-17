68f8f6b411d53e1ed73a9db8737f5c075284fc99
ypl <ypl238@nyu.edu>
2018-05-17 04:14:33 -0400

    fix the ident

--- RanFrst_regres.py
+++ RanFrst_regres.py
@@ -20,10 +20,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-from sklearn.feature_extraction import DictVectorizer
->> > v = DictVectorizer(sparse=False)
->> > D = [{'foo': 1, 'bar': 2}, {'foo': 3, 'baz': 1}]
->> > X = v.fit_transform(D)
+# from sklearn.feature_extraction import DictVectorizer
+# >> > v = DictVectorizer(sparse=False)
+# >> > D = [{'foo': 1, 'bar': 2}, {'foo': 3, 'baz': 1}]
+# >> > X = v.fit_transform(D)
 
 
 class RF_regression:
